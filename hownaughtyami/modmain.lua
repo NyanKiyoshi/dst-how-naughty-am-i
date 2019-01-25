60eb2efa37ad83d940f602be28f0d6294684a05f
NyanKiyoshi <hello@vanille.bid>
2019-01-25 21:52:29 +0100

    Add files via upload

--- hownaughtyami/modmain.lua
+++ hownaughtyami/modmain.lua
@@ -1,6 +1,5 @@
 local COMMAND_NAME = "naughty"
 local require = GLOBAL.require
-local kramped = require("components/kramped")
 
 local function say(user, say_string)
     print(user.name .. ": " .. say_string)
@@ -9,32 +8,12 @@ local function say(user, say_string)
     end)
 end
 
-kramped.GetDebugStringForPlayer = function (playerdata)
-    if playerdata.actions and playerdata.threshold and playerdata.timetodecay then
-        return string.format(
-            "Have %d / %d naughty actions (decay in %2.2f)...", 
-            playerdata.actions, playerdata.threshold, playerdata.timetodecay)
-    else
-        return "I'm calm."
-    end
-end
-
 local function tellPlayerNaughtiness(player)
     local worldsKramped = GLOBAL.TheWorld.components.kramped
     local message = worldsKramped:GetDebugString()
     say(player, message)
-
-    --if message == nil then
-    --    say(player, "No data found.")
-    --else
-    --    say(player, message)
-    --end
 end
 
-AddComponentPostInit("kramped", function (inst)
-    local _activeplayers = {}
-    inst.activePlayers = _activeplayers
-end)
 
 GLOBAL.AddModUserCommand("naughty mod", COMMAND_NAME, {
     --aliases = swaggies,
