2c9e4670df06d07207316fda9ba84fd9c94caaa0
NyanKiyoshi <hello@vanille.bid>
2019-01-25 21:50:48 +0100

    Add files via upload

---
+++ hownaughtyami/modinfo.lua
@@ -0,0 +1,19 @@
+-- This information tells other players more about the mod
+name = "How naughty am I?"
+description = "/naughty will tell you."
+author = "Izawu"
+version = "1.0"
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+dst_compatible = true
+
+--This lets clients know if they need to get the mod from the Steam Workshop to join the game
+all_clients_require_mod = false
+
+--This determines whether it causes a server to be marked as modded (and shows in the mod list)
+client_only_mod = false
+
+-- Can specify a custom icon for this mod!
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
